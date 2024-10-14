0fa3808b430cd06c780de3dd273f0a32104e1ce7
Fernanda <Fernanda@DESKTOP-6KUU3HJ>
2024-10-14 16:27:43 -0300

    cs

---
+++ src/tpintegrador/ObraSocial.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpintegrador;
+
+/**
+ *
+ * @author Fernanda
+ */
+public class ObraSocial {
+    private String nombre;
+    private int descuento;
+    
+    public void consultarCobertura(){
+        
+    }
+}
