e8272a1da66f9bcf9b9b603459851ed15fe6ccc3
Fernanda <Fernanda@DESKTOP-6KUU3HJ>
2024-10-14 16:49:56 -0300

    dsfdsf

---
+++ src/tpintegrador/AtencionMedica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpintegrador;
+
+/**
+ *
+ * @author Fernanda
+ */
+public abstract class AtencionMedica extends Paciente {
+
+    public AtencionMedica(int idPaciente, String sintomas, String nombre, String apellido, int dni, String fechaDeNacimiento) {
+        super(idPaciente, sintomas, nombre, apellido, dni, fechaDeNacimiento);
+    }
+    public void cancelarTurno(){
+        
+    }
+    public void agregarTurno(){
+        
+    }
+    
+}
